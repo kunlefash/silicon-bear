255b026af3a79ae6a85c9c46cc4b425832556334
kunlefash <kunlefashmayop@gmail.com>
2022-05-21 02:18:01 -0300

    media queries for the login page

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -30,23 +30,23 @@ function Signup() {
                 <h5>
                   First Name
                 </h5>
-                <input type="text" name="FirstName" value={firstName} onChange={(e)=>{setfirstName(e.target.value)}} id="firstName" />                
+                <input placeholder='First name' type="text" name="FirstName" value={firstName} onChange={(e)=>{setfirstName(e.target.value)}} id="firstName" />                
                 <h5>
                   Last Name
                 </h5>
-                <input type="text" name="LastName" value={lastName} onChange={(e)=>{setlastName(e.target.value)}} id="lastName" />                
+                <input type="text" placeholder='Last name' name="LastName" value={lastName} onChange={(e)=>{setlastName(e.target.value)}} id="lastName" />                
                 <h5>
-                  email
+                  Email
                 </h5>
-                <input type="Email" name="email" value={email} onChange={(e)=>{setemail(e.target.value)}} id="email" />                
+                <input type="Email" placeholder='Email' name="email" value={email} onChange={(e)=>{setemail(e.target.value)}} id="email" />                
                 <h5>
                   Password
                 </h5>
-                <input type="password" value={password1} onChange={(e)=>{setpassword1(e.target.value)}} name="Password" id="password1" />                
+                <input type="password" placeholder='Password' value={password1} onChange={(e)=>{setpassword1(e.target.value)}} name="Password" id="password1" />                
                 <h5>
                   Confirm password
                 </h5>
-                <input type="password" value={password2} onChange={(e)=>{setpassword2(e.target.value)}} name="Password" id="password2" />
+                <input type="password" placeholder='Confirm Password' value={password2} onChange={(e)=>{setpassword2(e.target.value)}} name="Password" id="password2" />
               </div>
               <div className="button__parent">
               <button type='submit' className='signup__button'>Sign Up</button>
